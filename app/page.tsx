4961632649356dcfbad9cdbd830d6d9b64d15183
Commondore <alex.som86@gmail.com>
2025-10-11 11:29:03 +0600

    clean branch

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,3 @@
-import {Header} from "@/components/header/header";
-import {Greeting} from "@/components/greeting/greeting";
-
 export default function HomePage() {
-	return (
-		<>
-			<Header />
-			<Greeting pageName="Главная" value="Салам"  />
-			<div>
-				<h1>Home page</h1>
-				<p>Описание</p>
-			</div>
-			<h3>Sub title</h3>
-		</>
-	)
+  return <div>Home page</div>;
 }
